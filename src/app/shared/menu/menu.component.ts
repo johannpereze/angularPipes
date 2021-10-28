c9ba4b01ed3fac3b0101a7708b33f6994fe9f69e
johannpereze <sebas.jsp@gmail.com>
2021-10-28 11:15:40 -0500

    Clase 140 agregamos los items al menu y sus rutas

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -4,41 +4,40 @@ import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class MenuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  items: MenuItem[] = [];
 
-  
-  items: MenuItem[] = []
-
-    ngOnInit() {
-        this.items = [
-            {
-                label: 'File',
-                items: [{
-                        label: 'New', 
-                        icon: 'pi pi-fw pi-plus',
-                        items: [
-                            {label: 'Project'},
-                            {label: 'Other'},
-                        ]
-                    },
-                    {label: 'Open'},
-                    {label: 'Quit'}
-                ]
-            },
-            {
-                label: 'Edit',
-                icon: 'pi pi-fw pi-pencil',
-                items: [
-                    {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                    {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-                ]
-            }
-        ];
-    }
-
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Pipes de angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y fechas',
+            icon: 'pi pi-align-left',
+            routerLink: '/',
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+            routerLink: 'numeros',
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+            routerLink: 'no-comunes',
+          },
+        ],
+      },
+      {
+        label: 'Personalizados',
+        icon: 'pi pi-cog',
+      },
+    ];
+  }
 }
