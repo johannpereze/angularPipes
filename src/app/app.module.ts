aa0a2174565ce946ca858271a20f8fa2b572d404
johannpereze <sebas.jsp@gmail.com>
2021-10-28 22:00:26 -0500

    Clase 144 seteamos el locale format para date, currency y number

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
+//Cambiar el locale de la app
+import localeEsCo from '@angular/common/locales/es-CO';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEsCo);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, SharedModule, AppRouterModule, VentasModule],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-CO' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
